ca3bd309c3d7c677855317b76b6650edb9580532
tarapong <admphrao@gmail.com>
2018-04-19 05:52:57 +0700

    change host db for mac

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=warehouse',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=warehouse',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
