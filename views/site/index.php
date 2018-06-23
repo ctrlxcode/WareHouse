57a27ba721526ce3e9daccc2f78202b321f57167
Control X Team <ctrlxcode@gmail.com>
2018-06-23 15:35:34 +0700

    test chart home page

--- views/site/index.php
+++ views/site/index.php
@@ -1,4 +1,5 @@
 <?php
+use miloschuman\highcharts\Highcharts;
 
 /* @var $this yii\web\View */
 
@@ -81,6 +82,33 @@ $this->title = Yii::$app->name;
 <div class="row">
      <div class="col-lg-12">
              <h1 class="page-header"><i class="fa fa-wrench fa-fw"></i> 10 อันดับส่งซ่อม</h1>
+     <?php
+        echo Highcharts::widget([
+           'options' => [
+              'title' => ['text' => '10 อันดับส่งซ่อม'],
+              'xAxis' => [
+                 'categories' => [
+                                    'คอมพิวเตอร์ (Ward ชาย)', 
+                                    'คอมพิวเตอร์ (ห้องพักแพทย์)',
+                                    'เครื่องสำรองไฟ (ห้องบัตร)',
+                                    'เครื่อง EKG (ER)',
+                                    'เครื่องปรับอากาศ (ห้องประชุม)',
+                                    'เครื่องวัดความดัน (NCD)',
+                                    'คอมพิวเตอร์ (ห้องบัตร)',
+                                    'คอมพิวเตอร์ (ห้องชันสูตร)',
+                                    'คอมพิวเตอร์ (ห้องทันตกรรม)',
+                                    'เครื่องสำรองไฟ (OPD)'
+                                ]
+              ],
+              'yAxis' => [
+                 'title' => ['text' => 'จำนวน']
+              ],
+              'series' => [
+                 ['type' => 'column','name' => 'ครุภัณฑ์ที่ส่งซ่อม', 'data' => [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+              ]
+           ]
+        ]);
+     ?>
      </div>        
 </div>
 </div>
